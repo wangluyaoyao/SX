6b618511694ac0696f1f617d0df6940e9459b4c8
ganv <1541568991@qq.com>
2022-11-02 20:15:49 +0800

    甘伟

--- src/main/java/com/suixing/controller/CouponController.java
+++ src/main/java/com/suixing/controller/CouponController.java
@@ -20,9 +20,11 @@ public class CouponController {
     @Autowired
     private ICouponService iCouponService;
 
+    //加载优惠券
     @GetMapping("getAllCoupon")
     public ServerResponse getAllCoupno(){
         ServerResponse response = iCouponService.getCouponAll();
+
         System.out.println(response);
         return response;
     }
--- src/main/java/com/suixing/service/impl/UserCoupnoServiceImpl.java
+++ src/main/java/com/suixing/service/impl/UserCoupnoServiceImpl.java
@@ -10,12 +10,20 @@ import com.suixing.mapper.CouponMapper;
 import com.suixing.mapper.UserCoupnoMapper;
 import com.suixing.service.IUserCoupnoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.scripting.support.ResourceScriptSource;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
@@ -26,7 +34,7 @@ import java.util.Date;
  * @since 2022-10-03
  */
 @Service
-@Component  //将对象层(当前类)的对象 注入到IOC容器中
+//@Component  //将对象层(当前类)的对象 注入到IOC容器中
 public class UserCoupnoServiceImpl extends ServiceImpl<UserCoupnoMapper, UserCoupno> implements IUserCoupnoService {
     @Autowired
     private UserCoupnoMapper userCoupno;
@@ -34,37 +42,18 @@ public class UserCoupnoServiceImpl extends ServiceImpl<UserCoupnoMapper, UserCou
     private CouponMapper couponMapper;
     @Autowired
     private UserCoupnoMapper userCoupnoMapper;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
+    private DefaultRedisScript script;
 
-    @Override
-    public ServerResponse userRecCou(Integer userId, Integer couId) {
-        System.out.println("用户id:"+userId);
-        System.out.println("优惠卷id:"+couId);
-        //生成优惠券信息
-        UserCoupno usercoupon = new UserCoupno();
-        usercoupon.setUserCouNum(NumGeneration.creatCoupnoNum());
-        usercoupon.setCouId(couId);
-        usercoupon.setUserId(userId);
-        Date date = new Date();
-//        日期
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
-        usercoupon.setUserCouTime(calendar.getTime());
-        usercoupon.setUserCouStart(calendar.getTime());
-//      查询优惠卷的优惠周期
-        QueryWrapper<Coupon> couType = new QueryWrapper<>();
-        couType.eq("cou_id",couId);
-        couType.select("cou_cycle");
-        Coupon coupon =  couponMapper.selectOne(couType); //查询对应的优惠卷信息
-        System.out.println(coupon);
-        int day = Integer.parseInt(coupon.getCouCycle()); //获得优惠卷优惠周期天数
+    @PostConstruct
+    public void init(){
 
-        calendar.add(Calendar.DATE,day);//增加指定天数
-        usercoupon.setUserCouEnd(calendar.getTime()); //结束时间
-        usercoupon.setUserCouState("0"); // 0:未使用
-        return ServerResponse.success(getUserPoupno(userId,couId,usercoupon),usercoupon);
+        script = new DefaultRedisScript<>();
+        script.setResultType(Long.class);
+        script.setScriptSource(new ResourceScriptSource(new ClassPathResource("skillsecond.lua")));
     }
-
 
     @Override
     public ServerResponse getById(int userId) {
@@ -72,20 +61,113 @@ public class UserCoupnoServiceImpl extends ServiceImpl<UserCoupnoMapper, UserCou
         if (userCoupno !=null)
             return ServerResponse.success("领取成功",userCoupno);
         return ServerResponse.fail("领取失败",null);
-
     }
 
-    public String getUserPoupno(int userId,int couId,UserCoupno usercoupon){
+    //抢券高并发处理
+    @Override
+    public ServerResponse userRecCou(Integer userId, Integer couId) {
+        System.out.println("用户id:"+userId+"优惠卷id:"+couId);
+//      查询优惠卷的优惠周期
+        String result =   setRedis(userId,couId);
+//        if (coupon == null)
+//            ServerResponse.success("优惠券被抢光了",null);
+        QueryWrapper<Coupon> cidition = new QueryWrapper<>();
+        cidition.eq("cou_id",couId);
+//        cidition.select("cou_cycle","cou_amount");
+        Coupon coupon =  couponMapper.selectOne(cidition); //查询对应的优惠卷信息
+        UserCoupno usercoupon = caeatUserCou(coupon,userId); //用户领取的优惠券的信息生成
+        if (userCoupnoMapper.insert(usercoupon)>0){
+            System.out.println("用户优惠券信息添加成功");
+        }
+//        String newUserCou =  getUserPoupno(userId,couId,usercoupon,coupon); //向USER表添加数据
+        return ServerResponse.success(result,usercoupon);
+    }
+
+
+    //redis处理高并发
+    public String setRedis(int userId,int couId){
+        // 先查 redis
+        Coupon coupon = null;
+        String key  =  "skill_pro_"+couId;
+        String uuid = UUID.randomUUID().toString().replace("-","");
+        boolean isLock = redisTemplate.opsForValue().setIfAbsent(key,uuid,100, TimeUnit.SECONDS);
+        if (isLock){//某人获得了锁
+            System.out.println(Thread.currentThread().getName()+"抢到了该优惠券");
+            //查询优惠券信息
+            coupon = (Coupon)redisTemplate.opsForValue().get("coupon_"+couId);
+            System.out.println("在redis中查询到优惠券的数据"+coupon);
+            if (coupon == null)
+                return "优惠券被抢光了";
+            if (coupon.getCouAmount() == 0) {
+                return "优惠券被抢光了";
+            }else {
+                coupon.setCouAmount(coupon.getCouAmount()-1);
+                redisTemplate.opsForValue().set("coupon_"+couId,coupon,12,TimeUnit.HOURS);
+                //执行lua脚本 ，删除锁，保证原子性
+                Long execute = (Long) redisTemplate.execute(script, Arrays.asList(key),uuid);
+
+            }
+            updateUserCoupno(coupon);
+            return "领取成功";
+        }else { //加锁失败，过0.1秒再获得锁
+            try {
+                Thread.sleep(100);
+                coupon = (Coupon) redisTemplate.opsForValue().get("coupon_"+couId);
+                if (coupon != null && coupon.getCouAmount()>0){
+                    userRecCou(userId,couId);
+                    return null;
+                }else {
+                    return null;
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    //数据库信息修改
+    public void  updateUserCoupno(Coupon coupon){
+        couponMapper.updateById(coupon);
+        System.out.println("数据库数据修改成功");
+    }
+    //数据库信息修改
+    public String getUserPoupno(int userId,int couId,UserCoupno usercoupon,Coupon coupon){
+        couponMapper.updateById(coupon);
         QueryWrapper<UserCoupno> queryWrapper  = new QueryWrapper<>();
         queryWrapper.eq("cou_id",couId);
         queryWrapper.eq("user_id",userId);
         queryWrapper.select("user_cou_id");
-        UserCoupno coupno = userCoupnoMapper.selectOne(queryWrapper);
-        if (coupno!= null)
-            return "你已经领取过该优惠券了";
-        if (userCoupnoMapper.insert(usercoupon)>0)
+//        UserCoupno coupno = userCoupnoMapper.selectOne(queryWrapper);
+//        if (coupno!= null)
+//            return "你已经领取过该优惠券了";
+        if (userCoupnoMapper.insert(usercoupon)>0) {
+            //更新数据库优惠见数量
+            Coupon newAmountCoupno =   new Coupon();
+            newAmountCoupno.setCouAmount(coupon.getCouAmount()-1);
+            if (coupon.getCouAmount()==0)
+                return "该优惠卷已经被领完了";
+            couponMapper.updateById(newAmountCoupno);
             return "领取成功";
-        return "领取失败";
+        }
+        return "数据修改成功";
+    }
+    //UserCoupno信息生成
+    public UserCoupno caeatUserCou(Coupon coupon,int userId){
+        //生成优惠券信息
+        UserCoupno usercoupon = new UserCoupno();
+        usercoupon.setUserCouNum(NumGeneration.creatCoupnoNum());
+        usercoupon.setCouId(coupon.getCouId());
+        usercoupon.setUserId(userId);
+        Date date = new Date();
+//        日期
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        usercoupon.setUserCouTime(calendar.getTime());
+        usercoupon.setUserCouStart(calendar.getTime());
+        int day = Integer.parseInt(coupon.getCouCycle()); //获得优惠卷优惠周期天数
+        calendar.add(Calendar.DATE,day);//增加指定天数
+        usercoupon.setUserCouEnd(calendar.getTime()); //结束时间
+        usercoupon.setUserCouState("0"); // 0:未使用
+        return usercoupon;
     }
 
 }
