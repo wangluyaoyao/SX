10e6c20bce926276bea73f4fe64eba5f09326ea1
fu4088 <2168373587@qq.com>
2022-11-02 14:22:30 +0800

    sword new new

--- src/main/java/com/suixing/service/ICommentsService.java
+++ src/main/java/com/suixing/service/ICommentsService.java
@@ -2,6 +2,7 @@ package com.suixing.service;
 
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.suixing.commons.ServerResponse;
 import com.suixing.entity.Comments;
 
 import java.util.List;
@@ -15,7 +16,8 @@ import java.util.List;
  * @since 2022-10-31
  */
 public interface ICommentsService extends IService<Comments> {
-
-    List<Comments> listComments();
-    int saveComment(Comments comments);
+    // 根据汽车id  分页查询评论 用户信息 和 评论信息 和 回复者信息及回复的内容
+    public ServerResponse getCommentsByCarId(Integer carId,Integer pageNum);
+    //点赞数量的获取
+//    public ServerResponse Up
 }
--- src/main/java/com/suixing/service/impl/CommentsServiceImpl.java
+++ src/main/java/com/suixing/service/impl/CommentsServiceImpl.java
@@ -2,9 +2,14 @@ package com.suixing.service.impl;
 
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.suixing.commons.ServerResponse;
 import com.suixing.entity.Comments;
 import com.suixing.mapper.CommentsMapper;
+import com.suixing.mapper.ReplyMapper;
+import com.suixing.mapper.UserMapper;
 import com.suixing.service.ICommentsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,13 +25,19 @@ import java.util.List;
 @Service
 public class CommentsServiceImpl extends ServiceImpl<CommentsMapper, Comments> implements ICommentsService {
 
-    @Override
-    public List<Comments> listComments() {
-        return null;
-    }
+    @Autowired
+    private CommentsMapper commentsMapper;
+
+    @Autowired
+    private ReplyMapper replyMapper;
+
+    @Autowired
+    private UserMapper userMapper;
+
 
     @Override
-    public int saveComment(Comments comments) {
-        return 0;
+    public ServerResponse getCommentsByCarId(Integer carId, Integer pageNum) {
+        Sort sort = Sort.by(Sort.Direction.DESC,"DEVID").and(Sort.by(Sort.Direction.DESC,"comm_time"));
+        return null;
     }
 }
